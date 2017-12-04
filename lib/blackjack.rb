9a5975a8b87d4b6add12774a08f28fd575c4a0cf
Matthew Welch <dmw9400@gmail.com>
2017-12-04 18:42:03 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -56,7 +56,7 @@ def runner
   while final_card_count < 21
     display_card_total(card_count)
     card_count = hit?(card_count)
-    return display_card_total (final_card_count)
+    return display_card_total (card_count)
   end
   end_game(final_card_count)
 end
